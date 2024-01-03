2064d60effeae2bc65777aa40ddf08c098f6a707
Nisha-Sagar <nishas842003@gmail.com>
2024-01-03 21:28:18 +0530

    initial commit

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,44 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audioplayers_web/audioplayers_web.dart';
+import 'package:camera_web/camera_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_app_check_web/firebase_app_check_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_sound_web/flutter_sound_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:record_web/record_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
+  CameraPlugin.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FirebaseAppCheckWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
+  FlutterSoundPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  RecordPluginWeb.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
